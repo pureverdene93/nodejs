d0f4d3079c42887f796bbd31298febac070fb143
25LP9922 25LP9922 <25LP9922@admins-iMac.local>
2025-12-01 13:18:40 +0800

    zail

--- src/resolver/foodOrder/foodOrder-userId.js
+++ src/resolver/foodOrder/foodOrder-userId.js
@@ -3,12 +3,12 @@ import { foodOrderModel } from "../../model/foodOrder-model.js";
 export const getFoodOrderByUsedrId = async (req, res) => {
   const userOrder = await foodOrderModel
     .find({ user: req.params.id })
-    .populate([
-      "foodOrderItem.food",
-      {
-        path: "user",
-        populate: "orderedFoods",
+    .populate({ path: "foodOrderItem.food" })
+    .populate({
+      path: "user",
+      populate: {
+        path: "adress",
       },
-    ]);
+    });
   res.json(userOrder);
 };
